a3dd69d6e3cc049fa35669a74adbf874875b1175
Bob Zhao <ybzhao1989@gmail.com>
2022-06-07 22:09:00 +0800

    fix update result twice

--- leN-carry/leN.js
+++ leN-carry/leN.js
@@ -2,35 +2,44 @@
 // let low = 0, high = 100
 // let result_max = 100
 let total_cnt = 100
+let correct_cnt = 0
+let modified_cnt = 0
 
 function rand_int(low, high, ones_low, ones_high) {
 	let i, ones
 	do {
 		i = parseInt(Math.random()*(high-low) + low)
 		ones = i % 10
-	} while (ones < ones_low || ones > ones_high)
+	} while (high > 10 && i <= 10 || ones < ones_low || ones > ones_high)
 	return i
 }
 
-$('#main').append('<div style="position: fixed; top: 0px; width: 170px; padding: 4px; color: white; background: teal; ">' +
-					'ok rate: <label id="ok_rate" ></label><br>' +
-					'time used: <label id="time_used"></label>' +
-					'</div>')
-// $('#main').append('<table style="margin-top: 3.5rem; "/>')
+$('#main').append(`<div style='position: fixed; top: 0px; width: 80%; padding: 4px; color: white; background: teal; '>
+ok rate: <label id='ok_rate' ></label><br>
+time used: <label id='time_used'></label>
+<div id='log'></log>
+</div>`)
+$('#main').append('<table/>')
 
 
 function update_input_handlers()
 {
 	let start
 	let last
-	function check_result(node, onfly) {
+	function check_result(node, onfly, modify) {
 		if (start === undefined) {
-			start = new Date().getTime()
+			start = Date.now()
 			last = start
+			// $('#log').append(`init start ${start}, last ${last}<br/>`)
 		}
-		let td_ret = node.parent()
-		let ok = node.val() == td_ret.data('result')
-		console.log('result ', ok, ' : ', node.val(), ' == ', td_ret.data('result'))
+
+		let td_answer = node.parent()
+		let ok = node.val() == td_answer.data('result')
+		// console.log('result ', ok, ' : ', node.val(), ' == ', td_answer.data('result'))
+		let row = node.parent().parent()
+		if (modify) {
+			node.data('modified', true)
+		}
 		if (onfly && !ok) {
 			return
 		}
@@ -39,13 +48,21 @@ function update_input_handlers()
 		node.prop('disabled', true)
 
 		// update statistics
-		let row = node.parent().parent()
-		row.children('.judge').html(`<font color=${ok && "green" || "red"}>${ok && "✅" || "❌"}</font>`).attr('ok', ok)
-		let len = $('.judge').filter((idx, elem) => { return $(elem).attr('ok') === 'true' }).length
-		$('#ok_rate').html(`${len}/${total_cnt}`)
+		row.children('.judge').html(`<font color=${ok && 'green' || 'red'}>${ok && '✅' || '❌'}</font>`)
+		if (node.data('modified')) {
+			modified_cnt++
+			row.children('.judge').css('border', '1px solid gray')
+		}
+		// let len = $('.judge').filter((idx, elem) => { return $(elem).attr('ok') === 'true' }).length
+		if (ok) {
+			correct_cnt++
+		}
+
+		$('#ok_rate').html(`${correct_cnt}/${total_cnt} (${modified_cnt} ?)`)
 
 		// time elapsed
-		let now = new Date().getTime()
+		let now = Date.now()
+		// $('#log').append(`now ${now} last ${last} diff ${now-last}, start ${start}<br/>`)
 		row.children('.time_used').html(`${Math.floor((now-last)/1000)}秒`)
 
 		let elapsed = (now - start)/1000
@@ -55,22 +72,24 @@ function update_input_handlers()
 		last = now
 
 		// focus next
-		let idx = td_ret.data('idx')
+		let idx = td_answer.data('idx')
 		$(`#result_${idx+1}`).focus()
 	}
 
 	function onkeyup(e) {
-		check_result($(e.target), true)
+		// console.log('keyCode ', e.keyCode, ' code ', e.code)
+		let modify = e.code == 'Backspace'
+		check_result($(e.target), true, modify)
 		return true
 	}
 
 	function onchange(e) {
-		check_result($(e.target), false)
+		check_result($(e.target), false, false)
 		return true
 	}
 
-	$('input.result').on('keyup', onkeyup)
-	// $('input.result').on('change', onchange)
+	$('input.answer_input').on('keyup', onkeyup)
+	// $('input.answer_input').on('change', onchange)  // enable this will update `last' twice, and result in elapsed 0
 }
 
 function gen_exam(result_max, ones_low, ones_high, minus_only, plus_only)
@@ -97,20 +116,20 @@ function gen_exam(result_max, ones_low, ones_high, minus_only, plus_only)
 				result = l1 - l2
 			}
 		} while (result < 0 || result > result_max)
-		let td = $('<td>')
-		td.append(`<label>${expr}</label>`)
+		let td_question = $('<td>')
+		td_question.append(`<label>${expr}</label>`)
 
-		let td_ret = $(`<td><input type="tel" id="result_${i}" class="result" style="width: 4em;"></td>`)
-		td_ret.data('expr',   expr)
-		td_ret.data('result', result)
-		td_ret.data('idx', i)
+		let td_answer = $(`<td><input type='tel' id='result_${i}' class='answer_input' style='width: 4em;'></td>`)
+		td_answer.data('expr',   expr)
+		td_answer.data('result', result)
+		td_answer.data('idx', i)
 
 		let tr = $('<tr/>')
-		tr.append(`<td style="text-align: center; color: white; background: gray;">${i}</font></td>`)
-		tr.append('<td class="judge" style="width: 1em" />')
-		tr.append(td)
-		tr.append(td_ret)
-		tr.append(`<td class="time_used"></td>`)
+		tr.append(`<td style='text-align: center; color: white; background: gray;'>${i}</font></td>`)
+		tr.append(`<td class='judge' style='width: 1em' />`)
+		tr.append(td_question)
+		tr.append(td_answer)
+		tr.append(`<td class='time_used'></td>`)
 		t.append(tr)
 	}
 
