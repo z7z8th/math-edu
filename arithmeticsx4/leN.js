0e62e1ae33628597ff1a72f86e3c50e6cddf81ef
Bob Zhao <ybzhao1989@gmail.com>
2023-05-16 21:59:44 +0800

    support divide with remainder

--- arithmeticsx4/leN.js
+++ arithmeticsx4/leN.js
@@ -56,7 +56,15 @@ function update_input_handlers()
 			return
 
 		let td_answer = node.parent()
-		let ok = node.val() == td_answer.data('result')
+		let remainder = td_answer.data('remainder')
+		let final_judge = !remainder || node.hasClass("remainder_input")
+
+		let ok = false
+		if (node.hasClass('answer_input'))
+			ok = node.val() == td_answer.data('result')
+		else
+			ok = node.val() == remainder && node.prev().val() == td_answer.data('result')
+
 		let row = node.parent().parent()
 		if (modify) {
 			node.data('modified', true)
@@ -70,32 +78,36 @@ function update_input_handlers()
 		node.data('ok', ok)
 
 		// update statistics
-		row.children('.judge').html(`<font color=${ok && 'green' || 'red'}>${ok && '✅' || '❌'}</font>`)
-		if (node.data('modified')) {
-			modified_cnt++
-			row.children('.judge').css('border', '1px solid gray').css('border-radius', '5px')
+		if (final_judge) {
+				row.children('.judge').html(`<font color=${ok && 'green' || 'red'}>${ok && '✅' || '❌'}</font>`)
+
+			if (node.data('modified')) {
+				modified_cnt++
+				row.children('.judge').css('border', '1px solid gray').css('border-radius', '5px')
+			}
+			// let len = $('.judge').filter((idx, elem) => { return $(elem).attr('ok') === 'true' }).length
+			if (ok) {
+				correct_cnt++
+			}
+			$('#ok_rate').html(`${correct_cnt}/${total_cnt} (${modified_cnt} ?)`)
+
+
+			// time elapsed
+			let now = Date.now()
+			// $('#log').append(`now ${now} last ${last} diff ${now-last}, start ${start}<br/>`)
+			row.children('.time_used').html(`${Math.floor((now-last)/1000)}秒`)
+
+			let elapsed = (now - start)/1000
+			let mins = Math.floor(elapsed/60)
+			let seconds = Math.floor(elapsed % 60)
+			$('#time_used').html(`${mins} m ${seconds} s`)
+			last = now
+			// focus next
+			let idx = td_answer.data('idx')
+			$(`#result_${idx+1}`).focus()
+		} else {
+			node.next().focus()
 		}
-		// let len = $('.judge').filter((idx, elem) => { return $(elem).attr('ok') === 'true' }).length
-		if (ok) {
-			correct_cnt++
-		}
-
-		$('#ok_rate').html(`${correct_cnt}/${total_cnt} (${modified_cnt} ?)`)
-
-		// time elapsed
-		let now = Date.now()
-		// $('#log').append(`now ${now} last ${last} diff ${now-last}, start ${start}<br/>`)
-		row.children('.time_used').html(`${Math.floor((now-last)/1000)}秒`)
-
-		let elapsed = (now - start)/1000
-		let mins = Math.floor(elapsed/60)
-		let seconds = Math.floor(elapsed % 60)
-		$('#time_used').html(`${mins}分${seconds}秒`)
-		last = now
-
-		// focus next
-		let idx = td_answer.data('idx')
-		$(`#result_${idx+1}`).focus()
 	}
 
 	function onkeyup(e) {
@@ -112,6 +124,8 @@ function update_input_handlers()
 
 	$('input.answer_input').on('keyup', onkeyup)
 	$('input.answer_input').on('change', onchange)  // enable this will update `last' twice, and result in elapsed 0
+	$('input.remainder_input').on('keyup', onkeyup)
+	$('input.remainder_input').on('change', onchange)  // enable this will update `last' twice, and result in elapsed 0
 }
 
 function reset_stat()
@@ -124,7 +138,7 @@ function reset_stat()
 	$('#ok_rate').html(``)
 }
 
-function gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_high, multidiv_units_high, plus_only, minus_only, multi_only, div_only)
+function gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_high, multidiv_units_high, plus_only, minus_only, multi_only, div_only, div_w_rem_only)
 {
 	let low = 0 //Math.min(10, result_max - 1)
 
@@ -154,26 +168,39 @@ function gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_hi
 			// log(`expr ${expr} ${result}`)
 			return [expr, result]
 		},
-		//*
+		//×
 		() => {
 			let l1 = rand_int(2, multidiv_units_high)
 			let l2 = rand_int(2, multidiv_units_high)
-			expr = `${l1} * ${l2} = `
+			expr = `${l1} × ${l2} = `
 			result = l1 * l2
 			// log(`expr ${expr} ${result}`)
 			return [expr, result]
 		},
-		//\/
+		//÷
 		() => {
 			do {
-				let l2 = rand_int(1, multidiv_units_high)
+				let l2 = rand_int(2, multidiv_units_high)
 				let l1 = rand_int(multidiv_units_high, multidiv_units_high*l2)
 				result = Math.floor(l1/l2)
 				l1 = l2 * result
-				expr = `${l1} / ${l2} = `
+				expr = `${l1} ÷ ${l2} = `
 			} while(result > multidiv_units_high)
 			// log(`expr ${expr} ${result}`)
 			return [expr, result]
+		},
+		//÷ with remainder
+		() => {
+			do {
+				let l2 = rand_int(2, multidiv_units_high)
+				let l1 = rand_int(multidiv_units_high, multidiv_units_high*l2)
+				result = Math.floor(l1/l2)
+				remainder = rand_int(1, l2-1)
+				l1 = l2*result + remainder
+				expr = `${l1} ÷ ${l2} = `
+			} while(result > multidiv_units_high)
+			// log(`expr ${expr} ${result}`)
+			return [expr, result, remainder]
 		}
 	]
 
@@ -181,8 +208,9 @@ function gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_hi
 		let tried = 0
 		let expr
 		let result
+		let remainder
 		do {
-			let fi = i % 4
+			let fi = i % 5
 			if (plus_only)
 				[expr, result] = gen_funcs[0]()
 			else if (minus_only)
@@ -191,16 +219,19 @@ function gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_hi
 				[expr, result] = gen_funcs[2]()
 			else if (div_only)
 				[expr, result] = gen_funcs[3]()
+			else if (div_w_rem_only)
+				[expr, result, remainder] = gen_funcs[4]()
 			else
-				[expr, result] = gen_funcs[fi]()
+				[expr, result, remainder] = gen_funcs[fi]()
 			// log(`out ${expr} ${result}`)
 		} while (++tried < 20 && (result < 0 || result > result_max))
 		let td_question = $('<td>')
 		td_question.append(`<label>${expr}</label>`)
 
-		let td_answer = $(`<td><input type='tel' id='result_${i}' class='answer_input' style='width: 4em;'></td>`)
+		let td_answer = $(`<td class=answer><input type='tel' id='result_${i}' class='answer_input' style='width: 4em;'>${remainder && "...<input type='tel' id='remainder_"+i+"' class='remainder_input' style='width: 4em;'>" || ""}</td>`)
 		td_answer.data('expr',   expr)
 		td_answer.data('result', result)
+		td_answer.data('remainder', remainder)
 		td_answer.data('idx', i)
 
 		let tr = $('<tr/>')
@@ -227,7 +258,19 @@ function gen_handler()
 	let minus_only = $('#minus_only').is(':checked')
 	let plus_only = $('#plus_only').is(':checked')
 	let multi_only = $('#multi_only').is(':checked')
-	let div_only = $('#div_only').is(':checked')	// console.log(`result_max ${typeof(result_max)}: ${result_max} minus_only ${minus_only}`)
+	let div_only = $('#div_only').is(':checked')
+	let div_w_rem_only = $('#div_w_rem_only').is(':checked')
+	// console.log(`result_max ${typeof(result_max)}: ${result_max} minus_only ${minus_only}`)
 	reset_stat()
-	gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_high, multidiv_units_high, plus_only, minus_only, multi_only, div_only)
+	gen_exam(result_max, l_units_low, l_units_high, r_units_low, r_units_high, multidiv_units_high, plus_only, minus_only, multi_only, div_only, div_w_rem_only)
+}
+
+// dark/white theme
+let h = new Date().getHours()
+if (h >= 17 || h<7) {
+	$('html').css('backgroundColor', 'black')
+	$('html').css('color', 'gray')
+} else {
+	$('html').css('backgroundColor', 'white')
+	$('html').css('color', 'black')
 }
